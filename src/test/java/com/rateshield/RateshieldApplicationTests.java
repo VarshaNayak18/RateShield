5d188c7b23f0501a58e0f6a4c7a3baa2a1a174a0
Varsha V Nayak <varshavnayak2003@gmail.com>
2026-08-17 19:58:19 +0530

    chore: initialize RateShield backend

---
+++ src/test/java/com/rateshield/RateshieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rateshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateshieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
